8c5bd7cea5f3ab8dad3b00b1ef6c02e23e0b2617
Leandrorodca <leandrorodca@gmail.com>
2020-03-28 18:17:25 -0300

    front-end concluído

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -15,7 +15,8 @@ module.exports = {
     },
 
     async create(request,response){
- 
+        
+      
      const {name , email, whatsapp, city , uf } = request.body;
 
      const id = crypto.randomBytes(4).toString('HEX');
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const routes = require('./routes');
 const cors = require('cors');
 
+
 const app = express();
 
 //para aceitar os parametros do request body
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use(cors();)
+
+
+
 
 
 app.listen(3333,()=>console.log('servidor rodando, porta 3333'));
